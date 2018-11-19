22a8f763efb2bb6a2b95e3a086c431e0fb69cafa
Mike <mchavez3@uw.edu>
2018-11-18 16:56:59 -0800

    Add User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User model
+const User = require("../../models/user");
+
 // @route     GET api/users/test
 // @desc      Tests users route
 // @access    Public
 router.get("/test", (req, res) => res.json({ msg: "User works" }));
+
+// @route     GET api/users/register
+// @desc      Register user
+// @access    Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists " });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.bodypassword
+      });
+    }
+  });
+});
 
 module.exports = router;
