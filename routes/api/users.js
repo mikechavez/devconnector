1fd2bbb5f84143d4c4cb3db0fcb346d4b4c87fe6
Mike <mchavez3@uw.edu>
2018-11-18 20:37:59 -0800

    fix typo in newUser constructor

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/user");
@@ -27,7 +28,20 @@ router.post("/register", (req, res) => {
         name: req.body.name,
         email: req.body.email,
         avatar,
-        password: req.bodypassword
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        try {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        } catch (err) {
+          console.log(err);
+        }
       });
     }
   });
