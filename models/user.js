c0cca49dcffb25e3ec08b488fd817b7c729cc7f5
Mike <mchavez3@uw.edu>
2018-11-18 18:57:49 -0800

    fix export statement and remove required attribute for avatar

--- models/user.js
+++ models/user.js
@@ -16,8 +16,7 @@ const UserSchema = new Schema({
     required: true
   },
   avatar: {
-    type: String,
-    required: true
+    type: String
   },
   date: {
     type: Date,
@@ -25,4 +24,4 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = User = mongoose.model("users", User);
+module.exports = User = mongoose.model("users", UserSchema);
