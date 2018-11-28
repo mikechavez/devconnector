7b38c965fc298acfcae6a32a0468448dba9b3450
Mike <mchavez3@uw.edu>
2018-11-27 22:12:14 -0800

    add jwt token to user login route

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://mike:01Mc0066@ds145043.mlab.com:45043/devconnector-mc"
+  mongoURI: "mongodb://mike:01Mc0066@ds145043.mlab.com:45043/devconnector-mc",
+  secret: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // Load User model
 const User = require("../../models/user");
@@ -44,6 +46,42 @@ router.post("/register", (req, res) => {
         }
       });
     }
+  });
+});
+
+// @route     GET api/users/login
+// @desc      Login user / Returning JWT Token
+// @access    Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+        // sign token
+        jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token
+          });
+        });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
   });
 });
 
