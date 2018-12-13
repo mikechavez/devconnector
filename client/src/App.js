b78f421c25eecc87a416e34a23609c7392fb6252
Mike <mchavez3@uw.edu>
2018-12-12 16:35:13 -0800

    Remove unused Redux functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import store from "./store";
-
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
