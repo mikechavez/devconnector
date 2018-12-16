b6ffa6e663a9872ffa8e263d9e1e65310aae5a87
Mike <mchavez3@uw.edu>
2018-12-16 11:46:06 -0800

    Add persistent user-logged-in functionality with token in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -9,6 +14,16 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info an exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
