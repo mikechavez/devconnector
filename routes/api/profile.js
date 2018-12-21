df14ecefb9ea19ebef69aed586975250ea684c9e
Mike <mchavez3@uw.edu>
2018-12-20 17:52:25 -0800

    Add missing social profileFields in POST route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -129,7 +129,8 @@ router.post(
     if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
     if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
     if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
     Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
