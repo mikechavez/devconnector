5cea2b281be338c8f651bf68f2d2986ee282863d
Mike <mchavez3@uw.edu>
2019-02-06 11:12:01 -0800

    Undo changes made during debugging

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,14 +13,14 @@ const validateLoginInput = require("../../validation/login");
 // Load User model
 const User = require("../../models/User");
 
-// @route     GET api/users/test
-// @desc      Tests users route
-// @access    Public
-router.get("/test", (req, res) => res.json({ msg: "User works" }));
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
-// @route     POST api/users/register
-// @desc      Register user
-// @access    Public
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
@@ -35,36 +35,35 @@ router.post("/register", (req, res) => {
       return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: "200", // size
-        r: "pg", // rating
-        d: "mm" // default avatar, if no photo provided
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
       });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
       });
+
       bcrypt.genSalt(10, (err, salt) => {
-        try {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        } catch (err) {
-          console.log(err);
-        }
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
 });
 
-// @route     POST api/users/login
-// @desc      Login user / Returning JWT Token
-// @access    Public
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
@@ -76,7 +75,7 @@ router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  // Find the user by email
+  // Find user by email
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
@@ -84,15 +83,13 @@ router.post("/login", (req, res) => {
       return res.status(404).json(errors);
     }
 
-    // Check password
+    // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        const payload = {
-          id: user.id,
-          name: user.name,
-          avatar: user.avatar
-        };
-        // sign token
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
         jwt.sign(
           payload,
           keys.secretOrKey,
@@ -105,16 +102,16 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        errors.password = "Password Incorrect";
+        errors.password = "Password incorrect";
         return res.status(400).json(errors);
       }
     });
   });
 });
 
-// @route     GET api/users/current
-// @desc      Return current user
-// @access    Private
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
 router.get(
   "/current",
   passport.authenticate("jwt", { session: false }),
